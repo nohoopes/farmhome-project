6e31855cbda75f52f6a9ce89edd856c0c1612fa7
Minh Nhut <nguyenminhnhut.1577@gmail.com>
2023-05-21 11:31:31 +0700

    Update 21/05/2023 part 1

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,9 +1,13 @@
-export interface User {
-    id: string;
-    username: string;
-    password: string;
-    firstname: string;
-    lastname: string;
-    address: string;
-    phone: string;
-  }
+export class User {
+  static id: number = 0;
+  static username: string = '';
+  static password: string = '';
+  static firstName: string = '';
+  static lastName: string = '';
+  static birthDay: string = '';
+  static location: any ='';
+  static phone: string = '';
+  static email: string = '';
+  static avatar: string = '';
+  static status: any = '';
+}
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,16 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { places } from 'src/app/models/places';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['../../scss/index.scss'],
 })
-export class ProfileComponent implements OnInit{
+export class ProfileComponent implements OnInit {
+  //declare
 
   imageUrl: string = '../../../assets/fruit_example.png';
-  
+
   selectedFile!: File;
+
+  baseApiUrl: string = 'https://backendfarmhome-production.up.railway.app';
+
+  user = User;
+
+  loading: boolean = false;
 
   places = places;
   provinces!: any[];
@@ -21,11 +31,48 @@ export class ProfileComponent implements OnInit{
   selectedDistrict: any;
   selectedWard: any;
 
+  id: number = 0;
+  username: string = '';
+  password: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  birthDay: string = '';
+  address: any = '';
+  ward: any = '';
+  district: any = '';
+  province: any = '';
+  phone: string = '';
+  email: string = '';
+  avatar: string = '';
+  status: any = '';
+
+  //function
+
+  constructor(private http: HttpClient, private router: Router) {}
+
   ngOnInit() {
     this.provinces = this.places.map((place) => ({
       province: place.province,
       province_code: place.province_code,
     }));
+
+    this.loading = true;
+    this.http.get(this.baseApiUrl + '/user/id/' + 2).subscribe((res: any) => {
+      this.user = res;
+      this.id = res.id;
+      this.firstName = res.firstName;
+      this.lastName = res.lastName;
+      this.email = res.email;
+      this.phone = res.phone;
+      this.birthDay = res.birthDay;
+      this.avatar = res.avatar;
+      this.status = res.status.name;
+      this.address = res.location.address;
+      this.ward = res.location.ward.name;
+      this.district = res.location.ward.district.name;
+      this.province = res.location.ward.district.province.name;
+      this.loading = false;
+    });
   }
 
   onProvinceChange() {
@@ -52,5 +99,4 @@ export class ProfileComponent implements OnInit{
       this.imageUrl = reader.result as string;
     };
   }
-
 }
